c83b2f91376d986cf73dc40812f56ee248910f6a
Gaurang Zalariya <zgppatel@gmail.com>
2022-06-17 21:00:26 +0530

    Centering main heading

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2 className='text-center'>Let's get started!</h2>
       <Expenses items={expenses} />
     </div>
   );
