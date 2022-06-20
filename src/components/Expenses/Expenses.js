7c0bc940de4a087bb49405c004eea8a76893e508
Gaurang Zalariya <zgppatel@gmail.com>
2022-06-20 14:14:47 +0530

    Changing Default Year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,7 +8,7 @@ import './Expenses.css';
 
 const Expenses = (props) => {
 
-    const [filteredYear, setFilteredYear] = useState('2022');
+    const [filteredYear, setFilteredYear] = useState('2021');
 
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
